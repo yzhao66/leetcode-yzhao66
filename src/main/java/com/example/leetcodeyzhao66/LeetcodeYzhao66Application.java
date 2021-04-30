01f8fd6267e2961c5320f72c33952fa7ec41f38a
yangzhuohao <yangzhuohao@cecii.cn>
2021-04-30 08:49:41 +0800

    初次提交

---
+++ src/main/java/com/example/leetcodeyzhao66/LeetcodeYzhao66Application.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeyzhao66;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeYzhao66Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeYzhao66Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leetcodeyzhao66/LeetcodeYzhao66ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcodeyzhao66;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeYzhao66ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
